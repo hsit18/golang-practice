5ec8b525be00a8f80fd37ee817acf6a739841aa3
Harpreet Singh <hsit18@gmail.com>
2024-05-15 22:38:00 +0530

    deck.go added

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world");
+	fmt.Println("Starting main....");
+	cards := deck{"Ace Cards", newCard()}
+	cards = append(cards, "Six of Shades")
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds";
 }
