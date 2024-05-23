a1bb8606494b3020f007b25c36948402b64319fd
Singh, Harpreet (Harpreet Singh) <hsit18@gmail.com>
2024-05-23 12:01:55 +0530

    go terminal input added

--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
+	"bufio"
+	"cards/channeldemo"
 	"cards/deck"
+	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Starting main....", time.Now())
-	cards := deck.NewDeck()
-	cards.Shuffle()
-	fmt.Println("cards suffled....")
-	cards.Print()
 
-	hand, remainingDeck := deck.Deal(cards, 5)
-	hand.Print()
-	//fmt.Println("remainingDeck....")
-	//remainingDeck.Print()
-	remainingDeck.SaveToFile("cards.txt")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(`
+		Select Programs to run:
+			1. Fibonacci
+			2. Cards
+	`)
 
-	deckContent := deck.NewDeckFromFile("cards.txt")
-	fmt.Println("deckContent....")
-	deckContent.Print()
+	for scanner.Scan() {
+		fmt.Println("You selected: ", scanner.Text())
+		fmt.Println("*************************************************************************************************************")
+
+		switch scanner.Text() {
+		case "1":
+			channeldemo.ExecuteFibonacci()
+		case "2":
+			cards := deck.NewDeck()
+			cards.Shuffle()
+			fmt.Println("cards suffled....")
+			cards.Print()
+
+			hand, remainingDeck := deck.Deal(cards, 5)
+			hand.Print()
+			//fmt.Println("remainingDeck....")
+			//remainingDeck.Print()
+			remainingDeck.SaveToFile("cards.txt")
+
+			deckContent := deck.NewDeckFromFile("cards.txt")
+			fmt.Println("deckContent....")
+			deckContent.Print()
+		}
+
+	}
 }
