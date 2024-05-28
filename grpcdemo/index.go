60ddf5536b252dfb99a63320a281801378e512bd
Singh, Harpreet (Harpreet Singh) <hsit18@gmail.com>
2024-05-28 15:30:48 +0530

    GRPC createUser implemented

--- grpcdemo/index.go
+++ grpcdemo/index.go
@@ -3,15 +3,37 @@ package grpcdemo
 import (
 	pb "cards/grpcdemo/generated"
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type UserService struct {
-	pb.UserServiceServer
+	pb.UnimplementedUserServiceServer
+}
+
+type User struct {
+	gorm.Model
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+var DB *gorm.DB
+var err error
+
+func InitialMigration() {
+	DB, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Error conecting DB...")
+	}
+	DB.AutoMigrate(&User{})
 }
 
 func Execute() {
@@ -24,6 +46,7 @@ func Execute() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, &UserService{})
 	fmt.Printf("GRPC server started at %v", listen.Addr().String())
+	InitialMigration()
 	grpcErr := grpcServer.Serve(listen)
 	if grpcErr != nil {
 		log.Fatalf("Failed to start GRPC server %v", err)
@@ -31,14 +54,34 @@ func Execute() {
 
 }
 
-func (u *UserService) CreateUser(ctx context.Context, req *pb.UserRequest) (*pb.CreateUserResponse, error) {
+func (u *UserService) CreateUser(ctx context.Context, req *pb.CreeateUserRequest) (*pb.CreateUserResponse, error) {
+
+	fmt.Println(req)
+
+	user := req.GetUser()
+	data := User{
+		Firstname: user.GetFirstname(),
+		Lastname:  user.GetLastname(),
+		Email:     user.GetEmail(),
+	}
+
+	fmt.Printf("%+v\n", data)
+
+	res := DB.Create(&data)
+
+	fmt.Printf("AFTER DB insert: %+v\n", data)
+
+	if res.RowsAffected == 0 {
+		return nil, errors.New("failed to create user")
+	}
 
 	return &pb.CreateUserResponse{
-		ID:        1,
-		Firstname: "Hello",
-		Lastname:  "Singh",
-		Email:     "trest@adsad.com",
-		CreatedAt: "22-22-22",
+		User: &pb.User{
+			ID:        int32(data.ID),
+			Firstname: data.Firstname,
+			Lastname:  data.Lastname,
+			Email:     data.Email,
+		},
 	}, nil
 
 }
